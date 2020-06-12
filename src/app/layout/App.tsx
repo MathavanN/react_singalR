d918809558d8f9e25b95b575745c175596a15b7b
math2018avan <math2018avan@gmail.com>
2020-06-12 20:15:49 +0800

    Update App.tsx
    private route updated

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,47 +1,67 @@
-import React, { Fragment, FC, useContext, useEffect } from 'react'
-import ModalContainer from '../common/modals/ModalContainer'
-import { ToastContainer } from 'react-toastify'
-import { RouteComponentProps, withRouter, Route, Switch } from 'react-router-dom'
-import { RootStoreContext } from '../stores/rootStore'
-import { observer } from 'mobx-react-lite'
-import Home from '../../features/home/Home'
-import ActivityDashboard from '../../features/activities/ActivityDashboard'
-import { Header, Container } from 'semantic-ui-react'
-import ActivityChat from '../../features/activities/ActivityChat'
-import NotFound from './NotFound'
+import React, { Fragment, FC, useContext, useEffect } from "react";
+import ModalContainer from "../common/modals/ModalContainer";
+import { ToastContainer } from "react-toastify";
+import {
+  RouteComponentProps,
+  withRouter,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { RootStoreContext } from "../stores/rootStore";
+import { observer } from "mobx-react-lite";
+import Home from "../../features/home/Home";
+import ActivityDashboard from "../../features/activities/ActivityDashboard";
+import { Container } from "semantic-ui-react";
+import ActivityChat from "../../features/activities/ActivityChat";
+import NotFound from "./NotFound";
+import NavBar from "../../features/nav/NavBar";
+import { LoadingComponent } from "./LoadingComponent";
+import PrivateRoute from "./PrivateRoute";
 
-const App: FC<RouteComponentProps> = ({ location }) => {
-  const rootStore = useContext(RootStoreContext)
-  const { setAppLoaded, token } = rootStore.commonStore
-  const { getUser, user } = rootStore.userStore;
+const App: FC<RouteComponentProps> = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { setAppLoaded, token, appLoaded } = rootStore.commonStore;
+  const { getUser } = rootStore.userStore;
   useEffect(() => {
-    console.log(token)
     if (token) {
-      getUser().finally(() => setAppLoaded())
+      getUser().finally(() => setAppLoaded());
+    } else {
+      setAppLoaded();
     }
-    else {
-      setAppLoaded()
-    }
-  }, [token, getUser, setAppLoaded])
+  }, [token, getUser, setAppLoaded]);
+
+  if (!appLoaded)
+    return <LoadingComponent content="Loading SignalR Demo App" />;
   return (
     <Fragment>
       <ModalContainer />
-      <ToastContainer position='bottom-right' />
-      <Header as='h2' disabled textAlign='center'>
-        Test SignalR App {user && `(Current User is ${user!.userName})`}
-      </Header>
-
-      <Container style={{ marginTop: '3em' }}>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/activities' exact component={ActivityDashboard} />
-          <Route path='/activity/:id' exact component={ActivityChat} />
-          <Route component={NotFound} />
-        </Switch>
-      </Container>
-
+      <ToastContainer position="bottom-right" />
+      <NavBar />
+      <Route path="/" exact component={Home} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <Container style={{ marginTop: "5em" }}>
+              <Switch>
+                <PrivateRoute
+                  path="/activities"
+                  exact
+                  component={ActivityDashboard}
+                />
+                <PrivateRoute
+                  path="/activity/:id"
+                  exact
+                  component={ActivityChat}
+                />
+                <Route component={NotFound} />
+              </Switch>
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
-  )
-}
+  );
+};
 
 export default withRouter(observer(App));
